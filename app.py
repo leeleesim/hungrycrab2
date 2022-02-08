1240287fc646f8c2213f5e17fa8463a6fbd918fa
leeleesim <97943003+leeleesim@users.noreply.github.com>
2022-02-08 09:00:06 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+#templates are used to separate bussiness logic from presentation logic
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+	return render_template('index.html')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
